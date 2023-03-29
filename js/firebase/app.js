92e12d881f3649d345b7bac8e1f836750dee13cc
Gabriel Aparecido da paixão <gabriel711906@hotmail.com>
2023-03-29 00:26:13 -0300

    add js firebase db

---
+++ js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAXEdurdFK0LBAT0uyqCgFJq_sQwDgFRjk",
+    authDomain: "projeto-stranger-things-ede19.firebaseapp.com",
+    projectId: "projeto-stranger-things-ede19",
+    storageBucket: "projeto-stranger-things-ede19.appspot.com",
+    messagingSenderId: "284912145306",
+    appId: "1:284912145306:web:65ff0a3309b3a15bdfa023"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
